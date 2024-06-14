b155106ad45c15804027c2a120eafa7ab6fe60a4
Minniti Julien <minnitijulien06@gmail.com>
2024-06-14 17:58:43 +0200

    (v0.14) RT-qPCR

--- pages/RT-qPCR.py
+++ pages/RT-qPCR.py
@@ -27,32 +27,84 @@ import math
 import pandas as pd
 
 
-local_test = platform.processor()
-
 # Settings for Streamlit page
 st.set_page_config(page_title="LabMaster", page_icon="🔬", initial_sidebar_state="expanded", layout="wide")
 
-# Main page
-st.logo("img/labmaster_logo.png")
-st.sidebar.image("img/labmaster_logo.png")
-st.sidebar.title('👩🏼‍🔬 LabMaster')
-st.sidebar.write("Created by Minniti Julien")
+# Sample table
+colm1, colm2, colm3, colm4 = st.columns([1, 0.5, 1, 1], gap="small")
+colm1.write("**Input user**")
 
-# Button sidebar
-colsb1, colsb2, colsb3 = st.sidebar.columns(3, gap="small")
-colsb1.link_button("Help ⁉", '')
-colsb2.link_button('GitHub', 'https://github.com/Jumitti/BlotMaster')
-if local_test == "":
-    colsb3.link_button('Download app 📥', 'https://github.com/Jumitti/BlotMaster/releases')
+initial_data = [
+    {"Sample name": "WT_1", "Conc. 1 (ng/µL)": 253.5, "Conc. 2 (ng/µL)": 256},
+    {"Sample name": "KO_1", "Conc. 1 (ng/µL)": 300, "Conc. 2 (ng/µL)": 302.2}
+]
+samples_table = pd.DataFrame(initial_data)
+mean_samples_table = colm1.data_editor(samples_table if 'rna_save' not in st.session_state
+                                       else st.session_state.rna_save[
+    ["Sample name", "Conc. 1 (ng/µL)", "Conc. 2 (ng/µL)"]],
+                                       num_rows="dynamic", hide_index=True)
+st.session_state['rna_samples_table_save'] = samples_table
+
+if "Conc. 1 (ng/µL)" in mean_samples_table.columns and "Conc. 2 (ng/µL)" in mean_samples_table.columns:
+    mean_samples_table["Mean (ng/µL)"] = mean_samples_table[["Conc. 1 (ng/µL)", "Conc. 2 (ng/µL)"]].mean(axis=1)
+elif "Conc. 1 (ng/µL)" in mean_samples_table.columns:
+    mean_samples_table["Mean (ng/µL)"] = mean_samples_table["Conc. 1 (ng/µL)"].mean(axis=1)
+elif "Conc. 2 (ng/µL)" in mean_samples_table.columns:
+    mean_samples_table["Mean (ng/µL)"] = mean_samples_table["Conc. 2 (ng/µL)"].mean(axis=1)
 else:
-    colsb3.link_button('Web app 🌐', 'https://blotmaster.streamlit.app/')
+    mean_samples_table["Mean (ng/µL)"] = 0
 
-# Table
-st.sidebar.divider()
-st.sidebar.page_link("labmaster.py", label="Home")
-st.sidebar.page_link("pages/westernblot.py", label="Western Blot")
-st.sidebar.page_link("pages/transfection.py", label="Transfection")
+colm2.write("**Summary and samples average**")
+st.session_state["rna_save"] = mean_samples_table
+mean_samples_table = mean_samples_table[["Sample name", "Mean (ng/µL)"]]
+colm2.dataframe(mean_samples_table, hide_index=True)
 
-# MIT licence
-st.sidebar.divider()
-st.sidebar.link_button('Under MIT licence', 'https://github.com/Jumitti/BlotMaster/blob/main/LICENSE')
+colm3.write("**Samples preparation**")
+elution_volume = colm3.number_input("Elution volume (µL)", value=30.00, min_value=0.01, step=0.01)
+experimental_rna = colm3.number_input("Experimental RNA (ng)", value=2000.00, min_value=0.01, step=0.01)
+primer = colm3.number_input("Primer (µL)", value=1.00, min_value=0.01, step=0.01)
+final_volume = colm3.number_input("Final volume (µL)", value=10, min_value=5, step=5)
+
+colm4.write("**After RT settings**")
+final_concentration = colm4.number_input("Final concentration (ng/µL)", value=20.00, min_value=0.01, step=0.01)
+
+st.divider()
+
+def sample_vol(row):
+    return experimental_rna / row["Mean (ng/µL)"]
+
+def h2o_after_rt(row):
+    if row["H2O (µL)"] >= 0:
+        return (experimental_rna / final_concentration) - (2 * final_volume)
+    else:
+        return (experimental_rna / final_concentration) - (2 * final_volume) + row["H2O (µL)"]
+
+
+# Output Table for Mix
+required_columns = ["Sample name", "Mean (ng/µL)"]
+if not set(required_columns).issubset(mean_samples_table.columns):
+    st.error("The columns 'Sample name' and 'Mean (ng/µL)' are required.")
+else:
+    missing_data = mean_samples_table[
+        mean_samples_table["Sample name"].isnull() | mean_samples_table["Mean (ng/µL)"].isnull()]
+    if not missing_data.empty or (mean_samples_table["Mean (ng/µL)"] < 0).any():
+        st.error("Some lines do not have concentrations or are negative.")
+    else:
+        adjusted_samples_table = mean_samples_table.copy()
+        adjusted_samples_table["Sample volume (µL)"] = adjusted_samples_table.apply(sample_vol, axis=1)
+        adjusted_samples_table["Primer (µL)"] = primer
+        adjusted_samples_table["H2O (µL)"] = final_volume - adjusted_samples_table["Sample volume (µL)"] - primer
+        adjusted_samples_table["RT mix (µL)"] = final_volume
+        adjusted_samples_table["H2O after RT (µL)"] = adjusted_samples_table.apply(h2o_after_rt, axis=1)
+
+        st.dataframe(adjusted_samples_table, hide_index=True)
+
+        RT_mix = [
+            {"Component": "GoScript 5X Reaction Buffer", "Volume (µL)": 2 * final_volume/5},
+            {"Component": "MgCl2 (25 mM)", "Volume (µL)": final_volume * 1.6 / 10},
+            {"Component": "dNTP (10 mM)", "Volume (µL)": final_volume / 10},
+            {"Component": "GoScript Reverse Transcriptase", "Volume (µL)": final_volume / 10},
+            {"Component": "H2O", "Volume (µL)": final_volume * 0.24}
+        ]
+        RT_mix_table = pd.DataFrame(RT_mix)
+        st.dataframe(RT_mix_table, hide_index=True)
